26f9795745ac32ab5f369d316df9dd07f1705b6c
Daniel <danieltsegay2286@gmail.com>
2025-01-26 21:41:02 +0300

    updated the server.js logic

--- frontend/server.js
+++ frontend/server.js
@@ -1,14 +1,19 @@
 // Import express and path modules.
 const express = require('express');
 const path = require('path');
+
 // Create an express app.
 const app = express();
+
 // Serve the static files from the React app.
-app.use(express.static(path.join(__dirname, 'dist')));
-// Redirect every request to index.html 
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Redirect every request to index.html.
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-  // Listen to the default port 80 
-  app.listen(80);
-  
+
+// Listen to the default port 80.
+app.listen(80, () => {
+  console.log('Server is running on port 80');
+});
